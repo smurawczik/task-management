beacba3378cf4fadbc0aa715439dc70abaa4a13b
smurawczik <sebastian.murawczik@gmail.com>
2023-08-24 22:02:38 +0200

    better models, need to update from _copilot_model file

--- _copilot_model.ts
+++ _copilot_model.ts
@@ -1,301 +1,247 @@
-// import {
-//   Entity,
-//   PrimaryGeneratedColumn,
-//   Column,
-//   OneToMany,
-//   ManyToOne,
-//   ManyToMany,
-//   JoinTable,
-// } from 'typeorm';
-
-// @Entity()
-// class Project {
-//   @PrimaryGeneratedColumn()
-//   id: number;
-
-//   @Column()
-//   name: string;
-
-//   @Column()
-//   description: string;
-
-//   @OneToMany(() => Sprint, (sprint) => sprint.project)
-//   sprints: Sprint[];
-// }
-
-// @Entity()
-// class User {
-//   @PrimaryGeneratedColumn()
-//   id: number;
-
-//   @Column({ unique: true })
-//   email: string;
-
-//   @Column()
-//   password: string;
-
-//   @Column()
-//   firstName: string;
-
-//   @Column()
-//   lastName: string;
-
-//   @ManyToMany(() => UserRoles, (userRoles) => userRoles.users)
-//   @JoinTable({
-//     name: 'user_has_roles',
-//   })
-//   roles: UserRoles[];
-
-//   @OneToMany(() => Task, (task) => task.reporter)
-//   reportedTasks: Task[];
-
-//   @OneToMany(() => Task, (task) => task.assignee)
-//   assignedTasks: Task[];
-
-//   @OneToMany(() => Issue, (issue) => issue.reporter)
-//   reportedIssues: Issue[];
-
-//   @OneToMany(() => Issue, (issue) => issue.assignee)
-//   assignedIssues: Issue[];
-
-//   @OneToMany(() => Issue, (issue) => issue.assignee)
-//   watchingIssues: Issue[];
-
-//   @ManyToMany(() => Organization, (organization) => organization.members)
-//   organizations: Organization[];
-
-//   @OneToMany(() => Comment, (comment) => comment.author)
-//   comments: Comment[];
-// }
-
-// @Entity()
-// export class UserRoles {
-//   @PrimaryGeneratedColumn()
-//   id: number;
-
-//   @Column()
-//   name: string;
-
-//   @ManyToMany(() => User, (user) => user.roles)
-//   users: User[];
-// }
-
-// @Entity()
-// class Sprint {
-//   @PrimaryGeneratedColumn()
-//   id: number;
-
-//   @Column()
-//   name: string;
-
-//   @Column()
-//   startDate: Date;
-
-//   @Column()
-//   endDate: Date;
-
-//   @ManyToOne(() => Project, (project) => project.sprints)
-//   project: Project;
-
-//   @OneToMany(() => Issue, (issue) => issue.sprint)
-//   issues: Issue[];
-
-//   @OneToMany(() => Task, (task) => task.sprint)
-//   tasks: Task[];
-// }
-
-// @Entity()
-// class Issue {
-//   @PrimaryGeneratedColumn()
-//   id: number;
-
-//   @Column()
-//   name: string;
-
-//   @Column({ default: IssueStatus.TO_DO })
-//   status: IssueStatus;
-
-//   @Column({ default: Priority.LOW })
-//   priority: Priority;
-
-//   @ManyToOne(() => Sprint, (sprint) => sprint.issues)
-//   sprint: Sprint;
-
-//   @ManyToOne(() => User, (user) => user.reportedIssues)
-//   reporter: User;
-
-//   @ManyToOne(() => User, (user) => user.assignedIssues)
-//   assignee: User;
-
-//   @OneToMany(() => Comment, (comment) => comment.issue)
-//   comments: Comment[];
-
-//   @ManyToMany(() => Label, (label) => label.issues)
-//   @JoinTable({
-//     name: 'issue_has_labels',
-//   })
-//   labels: Label[];
-
-//   @ManyToMany(() => User, (user) => user.watchingIssues)
-//   @JoinTable({
-//     name: 'issue_has_watchers',
-//   })
-//   watchers: User[];
-// }
-
-// @Entity()
-// class Task {
-//   @PrimaryGeneratedColumn()
-//   id: number;
-
-//   @Column()
-//   title: string;
-
-//   @Column({ nullable: true })
-//   description: string;
-
-//   @Column({ default: TaskStatus.TO_DO })
-//   status: TaskStatus;
-
-//   @Column({ default: Priority.LOW })
-//   priority: Priority;
-
-//   @Column()
-//   dueDate: Date;
-
-//   @Column({ default: () => 'CURRENT_TIMESTAMP' })
-//   createdAt: Date;
-
-//   @Column({ default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
-//   updatedAt: Date;
-
-//   @ManyToOne(() => Sprint, (sprint) => sprint.tasks)
-//   sprint: Sprint;
-
-//   @ManyToOne(() => User, (user) => user.assignedTasks)
-//   assignee: User;
-
-//   @ManyToOne(() => User, (user) => user.reportedTasks)
-//   reporter: User;
-
-//   @OneToMany(() => Comment, (comment) => comment.task)
-//   comments: Comment[];
-
-//   @ManyToMany(() => Label, (label) => label.issues)
-//   @JoinTable({ name: 'task_has_labels' })
-//   labels: Label[];
-
-//   @ManyToOne(() => Board, (board) => board.tasks)
-//   board: Board;
-// }
-
-// @Entity()
-// class Comment {
-//   @PrimaryGeneratedColumn()
-//   id: number;
-
-//   @Column()
-//   text: string;
-
-//   @Column({ default: () => 'CURRENT_TIMESTAMP' })
-//   createdAt: Date;
-
-//   @Column({ default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
-//   updatedAt: Date;
-
-//   @ManyToOne(() => User, (user) => user.comments)
-//   author: User;
-
-//   @ManyToOne(() => Task, (task) => task.comments)
-//   task: Task;
-
-//   @ManyToOne(() => Issue, (issue) => issue.comments)
-//   issue: Issue;
-// }
-
-// @Entity()
-// class Organization {
-//   @PrimaryGeneratedColumn()
-//   id: number;
-
-//   @Column({ unique: true })
-//   name: string;
-
-//   @ManyToMany(() => User, (user) => user.organizations)
-//   @JoinTable({
-//     name: 'organization_has_members',
-//   })
-//   members: User[];
-
-//   @OneToMany(() => Board, (board) => board.organization)
-//   boards: Board[];
-// }
-
-// @Entity()
-// class Board {
-//   @PrimaryGeneratedColumn()
-//   id: number;
-
-//   @Column()
-//   name: string;
-
-//   @Column({ nullable: true })
-//   description: string;
-
-//   @Column()
-//   dueDate: Date;
-
-//   @Column({ default: () => 'CURRENT_TIMESTAMP' })
-//   createdAt: Date;
-
-//   @Column({ default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
-//   updatedAt: Date;
-
-//   @OneToMany(() => Task, (task) => task.board)
-//   tasks: Task[];
-
-//   @ManyToOne(() => Organization, (organization) => organization.boards)
-//   organization: Organization;
-// }
-
-// @Entity()
-// class Label {
-//   @PrimaryGeneratedColumn()
-//   id: number;
-
-//   @Column()
-//   name: string;
-
-//   @ManyToMany(() => Issue, (issue) => issue.labels)
-//   issues: Issue[];
-
-//   @ManyToMany(() => Task, (task) => task.labels)
-//   tasks: Task[];
-// }
-
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class userRoles1691698835058 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {
-    // create a query to add user roles
-    await queryRunner.query(`
-            insert into user_roles (id, name) values ('1', 'admin');
-            insert into user_roles (id, name) values ('2', 'user');
-        `);
-  }
-
-  public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('user_roles');
-  }
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  OneToMany,
+  ManyToMany,
+  JoinTable,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @ManyToMany(() => Project, (project) => project.users)
+  @JoinTable()
+  projects: Project[];
+
+  @OneToMany(() => Issue, (issue) => issue.assignee)
+  assignedIssues: Issue[];
+
+  @OneToMany(() => Issue, (issue) => issue.reporter)
+  reportedIssues: Issue[];
 }
 
-import { DataSource } from 'typeorm';
+@Entity()
+export class Project {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-export default new DataSource({
-  type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'password',
-  database: 'task_management',
-});
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @OneToMany(() => Issue, (issue) => issue.project)
+  issues: Issue[];
+
+  @OneToMany(() => Sprint, (sprint) => sprint.project)
+  sprints: Sprint[];
+
+  @ManyToMany(() => User, (user) => user.projects)
+  users: User[];
+
+  @OneToMany(() => Board, (board) => board.project)
+  boards: Board[];
+}
+
+@Entity()
+export class Issue {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  type: string;
+
+  @Column()
+  status: string;
+
+  @ManyToOne(() => User, (user) => user.assignedIssues)
+  assignee: User;
+
+  @ManyToOne(() => User, (user) => user.reportedIssues)
+  reporter: User;
+
+  @ManyToOne(() => Project, (project) => project.issues)
+  project: Project;
+
+  @OneToMany(() => Comment, (comment) => comment.issue)
+  comments: Comment[];
+
+  @ManyToMany(() => Board, (board) => board.issues)
+  @JoinTable()
+  boards: Board[];
+
+  @ManyToMany(() => Component, (component) => component.issues)
+  @JoinTable()
+  components: Component[];
+
+  @ManyToMany(() => Version, (version) => version.issues)
+  @JoinTable()
+  versions: Version[];
+
+  @ManyToOne(() => Sprint, (sprint) => sprint.issues)
+  sprint: Sprint;
+
+  @ManyToOne(() => Priority, (priority) => priority.issues)
+  priority: Priority;
+}
+
+@Entity()
+export class Comment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  text: string;
+
+  @ManyToOne(() => User, (user) => user.id)
+  author: User;
+
+  @ManyToOne(() => Issue, (issue) => issue.comments)
+  issue: Issue;
+}
+
+@Entity()
+export class Sprint {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  startDate: Date;
+
+  @Column()
+  endDate: Date;
+
+  @ManyToOne(() => Project, (project) => project.sprints)
+  project: Project;
+
+  @OneToMany(() => Issue, (issue) => issue.sprint)
+  issues: Issue[];
+}
+
+@Entity()
+export class Board {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @ManyToOne(() => Project, (project) => project.boards)
+  project: Project;
+
+  @ManyToMany(() => Issue, (issue) => issue.boards)
+  issues: Issue[];
+}
+
+@Entity()
+export class Workflow {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @OneToMany(() => Status, (status) => status.workflow)
+  statuses: Status[];
+}
+
+@Entity()
+export class Status {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @ManyToOne(() => Workflow, (workflow) => workflow.statuses)
+  workflow: Workflow;
+
+  @OneToMany(() => Issue, (issue) => issue.priority)
+  issues: Issue[];
+}
+
+@Entity()
+export class Priority {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @OneToMany(() => Issue, (issue) => issue.priority)
+  issues: Issue[];
+}
+
+@Entity()
+export class Component {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @ManyToMany(() => Issue, (issue) => issue.components)
+  issues: Issue[];
+}
+
+@Entity()
+export class Version {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @ManyToMany(() => Issue, (issue) => issue.versions)
+  issues: Issue[];
+}
+
+@Entity()
+export class Attachment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  url: string;
+
+  @ManyToOne(() => Issue, (issue) => issue.id)
+  issue: Issue;
+}
+
+@Entity()
+export class Notification {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  message: string;
+
+  @ManyToOne(() => User, (user) => user.id)
+  user: User;
+}
